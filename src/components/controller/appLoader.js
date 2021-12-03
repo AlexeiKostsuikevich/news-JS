a94f5c426e6f1a3e4ddba719aeb621ad10c1ad28
AlexeiKostsuikevich <alexcompaq49@hotmail.com>
2021-12-03 19:41:50 +0300

    init: start task

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '11894e4841a94b9aaff73c41655e8400', // получите свой ключ https://newsapi.org/
         });
     }
 }
